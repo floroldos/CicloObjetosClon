1ea5a524e3adbc5d2929ca58d585717b33646f0e
floroldos <florencia.roldos@correo.ucu.edu.uy>
2022-08-24 23:18:17 -0300

    Comparacion de instancias

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -19,9 +19,9 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
-            /*en este caso se crean tantas instancias en el heap que el destructor empieza a borrar 
+            /*en el caso de 10.000.000, se crean tantas instancias en el heap que el destructor empieza a borrar 
             las que no están en uso para liberar espacios de memoria y crear unas nuevas.*/
-            for (int i = 0; i < 10000000; i++)
+            for (int i = 0; i < 100; i++)
             {
                 var Tren = new Train($"Trencito nro {i}");
             }
@@ -29,6 +29,12 @@ namespace ConsoleApplication
             var t1 = new Train( "Last Train To London");
             var t2 = new Train( "Last Train To London");
             var t3 = new Train("Runaway Train");
+            Console.WriteLine($"¿Es cierto que t1 == t2? --> {t1 == t2}");
+            Console.WriteLine($"¿Es cierto que t2 == t3? --> {t2 == t3}");
+            /*Como no hay dos o más variables que apuntan al mismo objeto, en ambos casos da False.
+            Además, en el caso de t1 y t2, solo comparten el nombre. Nunca las definimos como misma variable (t1 = t2),
+            por lo que son distintas e individuales.*/
+
         }
     }
 }
